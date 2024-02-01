e49dd86829c5f5b1851bbd1dd5db6ea971f994c1
osanwetechnologies <156021903+osanwetechnologies@users.noreply.github.com>
2024-01-31 16:39:58 -0800

    Update OpenReadCSV.py

--- OpenReadCSV.py
+++ OpenReadCSV.py
@@ -12,3 +12,22 @@ def get_user_age():
 
 def get_user_city():
   return input("Which city are you from? ")
+
+def get_user_position()
+  return input("What is your work position? ")
+
+def append_to_csv(row_data):
+  with open('positions.csv', 'a', newline='') as position:
+    writer = csv.writer(position)
+    print("Data appended to CSV file.")
+
+def main():
+  user_name = get_user_name()
+  user_age = get_user_age()
+  user_city = get_user_city()
+  user_position = get_user_position()
+  row_data = [user_name,user_age,user_city,user_position]
+  append_to_csv(row_data)
+
+if __name__ == "__main__":
+  main()
